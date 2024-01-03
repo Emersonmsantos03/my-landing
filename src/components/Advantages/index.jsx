359922d62a82e1d682426cb8459c1580aa3f204f
Emer Moraes <emermoraes03@gmail.com>
2024-01-03 13:40:34 +0000

    finalizando

--- src/components/Advantages/index.jsx
+++ src/components/Advantages/index.jsx
@@ -4,6 +4,7 @@ import './advantages.css'
 let Advantages = () => {
     return(
         <section className='advantages' id='advantages'>
+            <div className='advantages-container'>
             <div className='advantages-title'>
                 <h1>Vantagens de uma <b>Landing Page</b></h1>
             </div>
@@ -12,10 +13,12 @@ let Advantages = () => {
             <AdvCard />
             </div>
             <div className='advantages-bottom'>
-            <a href="https://wa.me/554198870232?text=Olá,%20Emerson!%20Gostaria%20de%20saber%20mais%20sobre%20os%20seus%20serviços!" target="_blank"><button>Impulsione o seu negócio!!</button></a>
-            </div>
+        <a href="https://wa.me/554198870232?text=Olá,%20Emerson!%20Gostaria%20de%20saber%20mais%20sobre%20os%20seus%20serviços!" target="_blank"><button>Impulsione o seu negócio!!</button></a>
+        </div>
+        </div>
 
         </section>
+        
 
     )
 }
