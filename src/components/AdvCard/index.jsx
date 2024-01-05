62b157ff3dd221532938c6bf7d48e9f6200f449f
Emer Moraes <emermoraes03@gmail.com>
2024-01-05 15:27:51 +0000

    adicionando telas médias

--- src/components/AdvCard/index.jsx
+++ src/components/AdvCard/index.jsx
@@ -7,7 +7,7 @@ import professional from '/public/img/icons/profissional.png'
 let AdvCard = () => {
     let infos = [
         {image: focus, title: 'Foco direcionado', info: 'A landing page concentra-se em um único objetivo, proporcionando aos visitantes uma experiência focada e direta, aumentando as chances de conversão.'},
-        {image:personal, title: 'Persinalização aprimorada', info: 'Permite adaptar a mensagem e o design de acordo com a audiência e a oferta, proporcionando uma experiência mais personalizada, o que pode resultar em maior engajamento e conversões.' },
+        {image:personal, title: 'Personalização aprimorada', info: 'Permite adaptar a mensagem e o design de acordo com a audiência e a oferta, proporcionando uma experiência mais personalizada, o que pode resultar em maior engajamento e conversões.' },
         {image: leads, title: 'Geração de Leads qualificados', info: 'Utilizando formulários de contato e chamadas à ação estratégicas, a landing page pode ajudar na captação de leads de qualidade, fornecendo à empresa informações valiosas sobre potenciais clientes de maneira profissional.' },
         {image: professional , title: 'Profissionalismo na apresentação', info: 'Uma landing page bem projetada transmite uma imagem profissional da empresa ou produto, estabelecendo confiança e credibilidade junto aos visitantes.'},
     ]
